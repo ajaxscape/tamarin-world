0f49f9947bf777fb4f8ae8d0a8b0c6119f0455e6
ajaxscape <ben@ajaxscape.com>
2016-06-14 09:50:14 +0100

    100% coverage for modules written so far

--- test/co-routines.spec.js
+++ test/co-routines.spec.js
@@ -16,29 +16,103 @@ describe('co-routines', function () {
   it('must have world', function () {
     expect(cor.getCoRoutines).to.throw('World must be defined')
   })
+
+  it('must have a valid timeout', function () {
+    const world = new TamarinWorld()
+    expect(() => cor.getCoRoutines(world, 'abc')).to.throw('Default Timeout must be a number')
+  })
+
   describe('valid world', function () {
-    var world
-    var coRoutines
-    var el
+    let world
+    let coRoutines
+    let el
+
+    let resolves = (world) => sinon.stub(world, 'getUntil').returns({
+      elementIsEnabled: () => Promise.resolve(true),
+      elementIsVisible: () => Promise.resolve(true),
+      elementIsNotVisible: () => Promise.resolve(true),
+      elementTextIs: () => Promise.resolve(true),
+      titleIs: () => Promise.resolve(true),
+      browserReady: () => Promise.resolve(true)
+    })
+
+    let rejects = (world) => sinon.stub(world, 'getUntil').returns({
+      elementIsEnabled: () => Promise.reject({message: 'Not Enabled'}),
+      elementIsVisible: () => Promise.reject({message: 'Not Visible'}),
+      elementIsNotVisible: () => Promise.reject({message: 'Not Visible'}),
+      elementTextIs: () => Promise.reject({message: 'Not Matching Text'}),
+      titleIs: () => Promise.reject({message: 'Not Matching Title'}),
+      browserReady: () => Promise.reject({message: 'Not Ready'})
+    })
 
     beforeEach(function () {
       world = new TamarinWorld()
-      sinon.stub(world, 'getUntil', () => ({
-        elementIsEnabled: () => true,
-        elementIsVisible: () => true,
-        elementTextIs: () => true,
-        titleIs: () => true,
-        browserReady: () => true
-      }))
-      coRoutines = cor.getCoRoutines(world)
       el = {
         getOuterHtml: () => Promise.resolve('')
       }
-
     })
 
-    it('whenVisible', function () {
-      return coRoutines.whenVisible(el).should.eventually.be.equal(true)
+    afterEach(function () {
+      world.getUntil.restore()
+    })
+
+    describe('resolved', function () {
+      beforeEach(function () {
+        resolves(world)
+        coRoutines = cor.getCoRoutines(world, 100)
+      })
+
+      it('whenVisible', function () {
+        return coRoutines.whenVisible(el).should.eventually.be.equal(el)
+      })
+
+      it('whenHidden', function () {
+        return coRoutines.whenHidden(el).should.eventually.be.equal(el)
+      })
+
+      it('whenMatches', function () {
+        return coRoutines.whenMatches(el).should.eventually.be.equal(el)
+      })
+
+      it('whenTitleIs', function () {
+        return coRoutines.whenTitleIs('title').should.eventually.be.equal(true)
+      })
+
+      it('whenBrowserReady', function () {
+        return coRoutines.whenBrowserReady().should.eventually.be.equal('about:blank')
+      })
+    })
+
+    describe('rejected', function () {
+      beforeEach(function () {
+        rejects(world)
+        coRoutines = cor.getCoRoutines(world)
+      })
+
+      it('whenVisible', function () {
+        return coRoutines.whenVisible(el)
+          .catch((err) => expect(err.message).to.contain('Not Enabled'))
+      })
+
+      it('whenHidden', function () {
+        return coRoutines.whenHidden(el)
+          .catch((err) => expect(err.message).to.contain('Not Visible'))
+      })
+
+      it('whenMatches', function () {
+        return coRoutines.whenMatches(el)
+          .catch((err) => expect(err.message).to.contain('Not Matching Text'))
+      })
+
+      it('whenTitleIs', function () {
+        return coRoutines.whenTitleIs(el)
+          .catch((err) => expect(err.message).to.contain('Not Matching Title'))
+      })
+
+      it('whenBrowserReady', function () {
+        return coRoutines.whenBrowserReady()
+          .catch((err) => expect(err.message).to.contain('Not Ready'))
+      })
     })
   })
 })
